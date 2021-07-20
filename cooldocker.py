4d04511c95f7f1371bcb9788f7888b5006b0a0bd
Lukas Jocham <lukas.jocham@boerse-go.de>
2021-07-20 10:20:05 +0200

    add data structure for entity

--- cooldocker.py
+++ cooldocker.py
@@ -2,10 +2,10 @@
 
 import argparse
 import docker
-from docker.types import DockerClient
-
 from tabulate import tabulate
 from termcolor import colored
+from dataclasses import dataclass
+from typing import Dict
 
 def __format_timedelta(time_str, pattern="%Y-%m-%dT%H:%M:%S"):
     from datetime import timedelta, datetime
@@ -15,16 +15,34 @@ def __format_timedelta(time_str, pattern="%Y-%m-%dT%H:%M:%S"):
         return f"> {delta.days} days ago"
     return f"{str(timedelta(seconds=int(delta.total_seconds())-3600))} ago"
 
-class DockerInfo:
+@dataclass
+class DockerEntity:
+    """
+    single entity information
+    """
+    name: str
+    cols: list
+    data: list
+    count: int
+
+class DockerParser:
     def __init__(self, client):
         self.client: DockerClient = client
+        self.ctx: dict = {
+                container: DockerEntity,
+                images: DockerEntity,
+                volumes: DockerEntity,
+                networks: DockerEntity,
+            }
 
-    def container_info(self):
-        client: DockerClient = self.client
-        container_data = []
-        for container in client.containers.list():
-            attrs = container.attrs
-            def get_container_ports():
+    def container(self) -> None:
+        for container in self.client.containers.list():
+            attrs: dict = container.attrs
+
+            # get all the ports for the container
+            # if multiple ports are exposed, used
+            # return a list of them
+            def get_container_ports() -> str:
                 ports = attrs["NetworkSettings"]["Ports"]
                 formatted_ports = ""
                 for port, mapping in ports.items():
@@ -34,19 +52,24 @@ class DockerInfo:
                     else:
                         formatted_ports += f"{port} "
                 return formatted_ports
-            def get_container_ip():
+
+            # get the container ip address
+            def get_container_ip() -> Dict[int, str] or None:
                 net_id = attrs["NetworkSettings"]["IPAddress"]
                 if net_id != "":
                     return net_id
                 if attrs["HostConfig"]["NetworkMode"] != "default":
                     network_name = attrs["HostConfig"]["NetworkMode"]
                     return attrs["NetworkSettings"]["Networks"][network_name]["IPAddress"]
+                return None
+
             def get_docker_status():
                 status = attrs["State"]["Status"]
                 if "Health" in attrs["State"]:
                     return f"{status} ({attrs['State']['Health']['Status']})"
                 return status
-            container_data.append([
+
+            container_data = [
                 attrs["Config"]["Hostname"],
                 attrs["Config"]["Image"],
                 __format_timedelta(time_str=attrs["Created"]),
@@ -54,65 +77,68 @@ class DockerInfo:
                 get_container_ports(),
                 attrs["Name"][1:],
                 get_container_ip(),
-            ])
-    
-        container_cols = [ "CONTAINER ID", "IMAGE", "CREATED", "STATUS", "PORTS", "NAMES", "IP ADDRESS" ]
-        return container_data, container_cols
+            ]
+            self.ctx["container"] = DockerEntity(
+                name="CONTAINER",
+                data=container_data,
+                count=len(container_data),
+                cols=["CONTAINER ID", "IMAGE", "CREATED", "STATUS", "PORTS", "NAMES", "IP ADDRESS" ]
+                )
 
-def image_info(client):
-    image_data = []
-    for image in client.images.list(filters={"dangling": False}):
-        attrs = image.attrs
-        def get_image_repo():
-           if len(attrs["RepoTags"]) >= 1:
-               return attrs["RepoTags"][0].split(":")[0]
-
-        def get_image_tag():
-           if len(attrs["RepoTags"]) >= 1:
-               return attrs["RepoTags"][0].split(":")[1]
-
-        def get_image_size():
-            return attrs["Size"]/8/(1024**2)
-
-        image_data.append([
-            get_image_repo(),
-            get_image_tag(),
-            __format_timedelta(time_str=attrs["Created"]),
-            get_image_size(),
-        ])
-
-    image_cols = [ "REPOSITORY", "TAG", "CREATED", "SIZE(MiB)" ]
-    return image_data, image_cols
-
-def net_info(client):
-    net_data = []
-    for net in client.networks.list(filters={"dangling": True}):
-        attrs = net.attrs
-        net_data.append([
-            attrs['Id'][:7],
-            attrs["Name"],
-            attrs["Driver"],
-            __format_timedelta(time_str=attrs["Created"]),
-            attrs["Scope"],
-            attrs["Internal"],
-            attrs["Attachable"],
-        ])
-
-    net_cols = [ "NET ID", "NAME", "DRIVER", "CREATED", "SCOPE", "INTERNAL", "ATTACHABLE"]
-    return net_data, net_cols
-
-def vol_info(client):
-    vol_data = []
-    for vol in client.volumes.list():
-        attrs = vol.attrs
-        vol_data.append([
-            attrs["Name"],
-            attrs["Driver"],
-            attrs["Scope"],
-        ])
-
-    vol_cols = [ "NAME", "DRIVER", "VOLUME", "SCOPE"]
-    return vol_data, vol_cols
+#def image_info(client):
+#    image_data = []
+#    for image in client.images.list(filters={"dangling": False}):
+#        attrs = image.attrs
+#        def get_image_repo():
+#           if len(attrs["RepoTags"]) >= 1:
+#               return attrs["RepoTags"][0].split(":")[0]
+#
+#        def get_image_tag():
+#           if len(attrs["RepoTags"]) >= 1:
+#               return attrs["RepoTags"][0].split(":")[1]
+#
+#        def get_image_size():
+#            return attrs["Size"]/8/(1024**2)
+#
+#        image_data.append([
+#            get_image_repo(),
+#            get_image_tag(),
+#            __format_timedelta(time_str=attrs["Created"]),
+#            get_image_size(),
+#        ])
+#
+#    image_cols = [ "REPOSITORY", "TAG", "CREATED", "SIZE(MiB)" ]
+#    return image_data, image_cols
+#
+#def net_info(client):
+#    net_data = []
+#    for net in client.networks.list(filters={"dangling": True}):
+#        attrs = net.attrs
+#        net_data.append([
+#            attrs['Id'][:7],
+#            attrs["Name"],
+#            attrs["Driver"],
+#            __format_timedelta(time_str=attrs["Created"]),
+#            attrs["Scope"],
+#            attrs["Internal"],
+#            attrs["Attachable"],
+#        ])
+#
+#    net_cols = [ "NET ID", "NAME", "DRIVER", "CREATED", "SCOPE", "INTERNAL", "ATTACHABLE"]
+#    return net_data, net_cols
+#
+#def vol_info(client):
+#    vol_data = []
+#    for vol in client.volumes.list():
+#        attrs = vol.attrs
+#        vol_data.append([
+#            attrs["Name"],
+#            attrs["Driver"],
+#            attrs["Scope"],
+#        ])
+#
+#    vol_cols = [ "NAME", "DRIVER", "VOLUME", "SCOPE"]
+#    return vol_data, vol_cols
 
 
 if __name__ == "__main__":
@@ -126,22 +152,26 @@ if __name__ == "__main__":
         client: DockerClient = docker.from_env()
         info: DockerInfo = DockerInfo(client=client)
 
-        container_info, container_cols = info.container()
-        print_counts("CONTAINER", "cyan", len(container_data))
-        print(tabulate(container_info, headers=container_cols))
 
-        image_data, image_cols = image_info(client)
-        print_counts("IMAGES", "green", len(image_data))
-        print(tabulate(image_data, headers=image_cols))
+        # ?? for x in info:
+        #     print_counts(x.name, x.color, x.count)
 
-        net_data, net_cols = net_info(client)
-        print_counts("NETS", "blue", len(net_data))
-        print(tabulate(net_data, headers=net_cols))
+        #container_info, container_cols = info.container()
+        #print_counts("CONTAINER", "cyan", len(container_info))
+        #print(tabulate(container_info, headers=container_cols))
 
-        vol_data, vol_cols = vol_info(client)
-        if len(vol_data) >= 1:
-            print_counts("VOLUMES", "yellow", len(vol_data))
-            print(tabulate(vol_data, headers=vol_cols))
+        #image_data, image_cols = image_info(client)
+        #print_counts("IMAGES", "green", len(image_data))
+        #print(tabulate(image_data, headers=image_cols))
+
+        #net_data, net_cols = net_info(client)
+        #print_counts("NETS", "blue", len(net_data))
+        #print(tabulate(net_data, headers=net_cols))
+
+        #vol_data, vol_cols = vol_info(client)
+        #if len(vol_data) >= 1:
+        #    print_counts("VOLUMES", "yellow", len(vol_data))
+        #    print(tabulate(vol_data, headers=vol_cols))
 
     except Exception as err:
         print(f"Docker Engine/Daemon not running. Please start it. ERR: {err}")
